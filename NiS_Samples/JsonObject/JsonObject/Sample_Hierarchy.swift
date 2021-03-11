71d6d96563f82b94eadd81e32e46779c6bb08756
Nine99 <nine99@live.co.kr>
2021-03-12 08:48:17 +0900

    Protocol Test 추가

--- NiS_Samples/JsonObject/JsonObject/Sample_Hierarchy.swift
+++ NiS_Samples/JsonObject/JsonObject/Sample_Hierarchy.swift
@@ -23,47 +23,118 @@ class C1B : SC0 {
     
 }
 
+class VNative {
+    var nName: String?
+}
+
+class VHybrid {
+    var hName: String?
+}
+
 protocol P0 {
-    func Create()
+    var classID: String? { get set }
+    var vClass: AnyObject? { get set }
+    
+    var classType: _C_TYPE { get set }
+    static func Create(_ cID: String) -> P0
+}
+
+extension P0 {
+    func getVClass<T>() -> T {
+        return vClass as! T
+    }
 }
 
 class C2Native : C1A, P0 {
-    func Create() {
+    var classID: String?
+    
+    var vClass: AnyObject?
+    
+    var classType: _C_TYPE = ._NATIVE
+    
+    static func Create(_ cID: String) -> P0 {
         Logger?.Log( "Create Native")
+        let cobj = C2Native()
+        cobj.classID = cID
+        cobj.vClass = VNative() as AnyObject
+        
+        return cobj
     }
-    
-    
-    
 }
 
 class C2Hybrid : C1B, P0 {
-    func Create() {
+    var classID: String?
+    
+    var vClass: AnyObject?
+    
+    var classType: _C_TYPE = ._HYBRID
+    
+    static func Create(_ cID: String) -> P0 {
         Logger?.Log( "Create Hybrid")
+        let cobj = C2Hybrid()
+        cobj.classID = cID
+        cobj.vClass = VHybrid() as AnyObject
+        
+        return cobj
     }
     
 }
+
+class C2Common: SC0, P0 {
+    var classID: String?
+    
+    var vClass: AnyObject?
+    
+    var classType: _C_TYPE
+    
+    static func Create(_ cID: String) -> P0 {
+        return C2Common(cID, type: ._NATIVE)
+    }
+    
+    init(_ cID: String?, type: _C_TYPE) {
+        self.classID = cID
+        self.classType = type
+        switch self.classType {
+        case ._NATIVE:
+            self.vClass = VNative() as AnyObject
+            break;
+            
+        case ._HYBRID:
+            self.vClass = VHybrid() as AnyObject
+            break;
+        }
+    }
+    
+//    func getVClass<T>() -> T {
+//        return vClass as! T
+//    }
+}
+
+typealias fnClassCreator = (String) -> P0
 
 protocol P2Delegate: class {
     var createDelegator: SC0? { get set }
     
-    func CreateByType(cID: String, type: _C_TYPE)
+    var fnCreate : Dictionary<_C_TYPE, fnClassCreator>? { get set }
+}
+
+extension P2Delegate {
+    
+    func Create(_ cID: String, type:_C_TYPE) -> P0 {
+        return fnCreate![type]!(cID)
+    }
 }
 
 class HManager: P2Delegate {
+    var fnCreate: Dictionary<_C_TYPE, fnClassCreator>? = Dictionary<_C_TYPE, fnClassCreator>()
+    
     var createDelegator: SC0?
 
-    func CreateByType(cID: String, type: _C_TYPE) {
-        switch type {
-            case ._NATIVE:
-                let cc = C2Native()
-                cc.Create()
-                break
-            case ._HYBRID:
-                let cc = C2Hybrid()
-                cc.Create()
-                break
-        }
+    func AddClassType(type: _C_TYPE, fn: @escaping fnClassCreator)
+    {
+        fnCreate!.updateValue(fn, forKey: type)
     }
+    
 }
 
 var mgrHier: HManager?
--- NiS_Samples/JsonObject/JsonObject/main_CmdFuncs+Protocol.swift
+++ NiS_Samples/JsonObject/JsonObject/main_CmdFuncs+Protocol.swift
@@ -9,14 +9,17 @@ import Foundation
 import NiSConsole
 
 var cmdNodes_Protocol: [NiCmdNode] = [
-    NiCmdNode("CALL",             fnCmd_Protocol_Call,            desc: "" ),
-    NiCmdNode("ADD",              fnCmd_Protocol_Add,             desc: "" )
+    NiCmdNode("INIT",               fnCmd_Protocol_Init,            desc: "" ),
+    NiCmdNode("CALL",               fnCmd_Protocol_Call,            desc: "" ),
+    NiCmdNode("ADD",                fnCmd_Protocol_Add,             desc: "" )
 ]
 
 var cmdExecutor_Protocol: NiSCmdExecutor<NiCmdNode> = NiSCmdExecutor<NiCmdNode>()
 
 func fnCmd_Protocol( cmd: [String?] ) -> _ACTION_RESULT {
     let _ = cmdExecutor_Protocol.AddCmd(_node: NiCmdNode("EXIT", {_ ->_ACTION_RESULT in
+        mgrHier = nil
+        
         return ._EXIT
     }))
     
@@ -24,7 +27,15 @@ func fnCmd_Protocol( cmd: [String?] ) -> _ACTION_RESULT {
     
     mgrHier = HManager()
     
+    mgrHier?.AddClassType(type: ._HYBRID, fn: C2Hybrid.Create(_:))
+    mgrHier?.AddClassType(type: ._NATIVE, fn: C2Native.Create(_:))
+    
     fwCon.CommandLoop(cmdExecutor_Protocol, levelStr: cmd[0]!)
+    
+    return ._OK
+}
+
+func fnCmd_Protocol_Init(cmd: [String?]) -> _ACTION_RESULT {
     
     return ._OK
 }
@@ -34,13 +45,47 @@ func fnCmd_Protocol_Add(cmd: [String?]) -> _ACTION_RESULT {
         return ._INVALIED_ARGUMENT
     }
     
-    mgrHier?.CreateByType(cID: "NewClass", type: type)
+    let prtcl = mgrHier?.Create("NewClass", type: type)
+    switch prtcl?.classType {
+    case ._HYBRID:
+        let obj = prtcl?.vClass as! VHybrid
+        obj.hName = "Hybrid Class"
+        Logger?.Log(obj.hName!)
+        break
+        
+    case ._NATIVE:
+        let obj = prtcl?.vClass as! VNative
+        obj.nName = "Native Class"
+        Logger?.Log(obj.nName!)
+        break
+        
+    default:
+        break
+    }
+    
+    Logger?.Log( "\(prtcl!.self)" )
     
     return ._OK
 }
 
 func fnCmd_Protocol_Call(cmd: [String?]) -> _ACTION_RESULT {
+    guard let type:_C_TYPE = _C_TYPE(rawValue: cmd[1]!) else {
+        return ._INVALIED_ARGUMENT
+    }
 
+    let prtcl = C2Common("Test Class", type: type )
+    switch type {
+    case ._NATIVE:
+        let cls: VNative = prtcl.getVClass()
+        cls.nName = "Native Class String"
+        Logger?.Log(cls.nName!)
+        break
+    case ._HYBRID:
+        let cls: VHybrid = prtcl.getVClass()
+        cls.hName = "Hybrid Class String"
+        Logger?.Log(cls.hName!)
+        break
+    }
     
     return ._OK
 }
